4cef9b42e14b796e63693d05e9823d96945419fa
skarrierr <david.reyes@enti.cat>
2022-11-29 10:28:52 +0100

    Cuarto push
    no he avanzao mucho  tampoco

--- VS_Unity/Assets/Scripts/Controllers/CanonController.cs
+++ VS_Unity/Assets/Scripts/Controllers/CanonController.cs
@@ -7,7 +7,12 @@ public class CanonController : MonoBehaviour
 
     public GameObject Camera;
     public bool aiming;
-    
+    public bool CanShoot;
+    public GameObject CanonBall;
+    public GameObject CanonSpawn;
+
+    public float tiempo;
+    public float CanonCooldown;
     void Start()
     {
        
@@ -16,11 +21,36 @@ public class CanonController : MonoBehaviour
     
     void Update()
     {
+
+        
+
         aiming = Camera.GetComponent<CameraController>().aiming;
 
         if (aiming)
         {
             this.transform.rotation = Camera.transform.rotation;
+
+
+            
+        
+        }
+        /* if (!CanShoot)
+         {
+             tiempo += Time.deltaTime;
+             if (tiempo >= CanonCooldown)
+             {
+                 tiempo = 0;
+                 CanShoot = true;
+             }
+         }*/
+
+        if (Input.GetKey(KeyCode.P) && CanShoot)
+        {
+            GameObject _canon = Instantiate(CanonBall, CanonSpawn.transform);
+            _canon.GetComponent<Rigidbody>().AddForce(_canon.transform.forward, ForceMode.Impulse);
+            _canon.GetComponent<Rigidbody>().AddForce(_canon.transform.up, ForceMode.Impulse);
+
+           // CanShoot = false;
         }
     }
 }
